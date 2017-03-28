96bccaa80fe22828a736930f41427e951afb694b
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2017-03-28 17:19:20 -0500

    assignment-7:completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -26,16 +26,25 @@
             
             currentSong = song;
         };
+         /**
+        * @function playSong
+        * @desc Plays song and sets playing to true on the sond object property of playing
+        * @param {Object} song
+        */
+        var playSong = function(song){
+            currentBuzzObject.play();
+            song.playing = true;
+            
+        }
         
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song)
                 
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
